3f22c5cedab68926b588a3ec96645e172021f9d7
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-01-21 20:12:14 +0300

    chore(eslint): add and configure perfectionist plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,39 @@
 // @ts-check
 
 import eslint from '@eslint/js'
+import perfectionist from 'eslint-plugin-perfectionist'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      'perfectionist/sort-imports': ['error', {
+        type: 'natural',
+        order: 'asc',
+        ignoreCase: false,
+        newlinesBetween: 'always',
+        environment: 'node',
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal-type',
+          'internal',
+          ['parent-type', 'sibling-type', 'index-type'],
+          ['parent', 'sibling', 'index'],
+          'object',
+          'unknown',
+        ],
+      }],
+      'perfectionist/sort-named-imports': ['error', { order: 'asc', type: 'alphabetical' }],
+    },
+  },
   {
     files: ['**/*.ts'],
     languageOptions: {
