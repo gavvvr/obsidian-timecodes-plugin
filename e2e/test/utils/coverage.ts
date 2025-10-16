4ac963acc8fb4d850b99d8c14117c0b501b8c6d2
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-10-17 00:23:15 +0300

    fix coverage reporting for e2e tests

--- e2e/test/utils/coverage.ts
+++ e2e/test/utils/coverage.ts
@@ -1,15 +1,29 @@
 import type { CoverageMapData } from 'istanbul-lib-coverage'
 
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
 import { createCoverageMap } from 'istanbul-lib-coverage'
 import { createContext } from 'istanbul-lib-report'
 import { createSourceMapStore } from 'istanbul-lib-source-maps'
 import * as reports from 'istanbul-reports'
 
+const cwd = process.cwd()
+
 export async function exportCoverageToLcov(lcovFileName: string) {
   const coverageMap = createCoverageMap(await collectCoverageData())
   const remappedCoverageMap = await createSourceMapStore().transformCoverage(coverageMap)
 
-  const context = createContext({ coverageMap: remappedCoverageMap, dir: '.' })
+  const context = createContext({
+    coverageMap: remappedCoverageMap,
+    dir: '.',
+    sourceFinder: (filePath: string) => {
+      const relativePart = path.relative(cwd, filePath)
+
+      const newAbsPath = path.join(path.resolve(cwd, '..'), relativePart)
+      return fs.readFileSync(newAbsPath, 'utf-8')
+    },
+  })
   const lcov = reports.create('lcovonly', { file: lcovFileName })
 
   lcov.execute(context)
