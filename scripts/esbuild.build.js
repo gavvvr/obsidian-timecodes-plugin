8c674b5dcf71ba62a2be093486a48f90ed89365f
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-10-20 00:31:36 +0300

    build: add task producing instrumented build for measuring code coverage
    #5

--- scripts/esbuild.build.js
+++ scripts/esbuild.build.js
@@ -5,20 +5,29 @@ import process from 'node:process'
 import esbuild from 'esbuild'
 
 import { sharedEsbuildConfig } from './esbuild.config.js'
+import { instrumentWithSourceMaps } from './utils/instrument.js'
 
 const config = sharedEsbuildConfig
 
 const prod = process.argv[2] === 'production'
+const coverage = process.argv[2] === 'coverage'
 
 const context = await esbuild.context({
   ...config,
   ...prod
     ? { minify: true, sourcemap: false }
     : {},
+  ...coverage
+    ? { minify: false, sourcemap: 'inline' }
+    : {},
 })
 
 if (prod) {
   await context.rebuild()
+  process.exit(0)
+} else if (coverage) {
+  await context.rebuild()
+  instrumentWithSourceMaps('out/main.js', 'out/main.js')
   process.exit(0)
 } else {
   await context.watch()
