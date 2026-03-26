46b76af4fdad383ed722f39e26ec00bdc5fb5f6c
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2026-03-26 22:13:07 +0300

    test(e2e): fix EPERM error during tests execution on Mac
    Fixes #7

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,2 +1,4 @@
+import * as path from 'node:path'
+
 export const TIMECODES_PLUGIN_ID = 'timecodes'
-export const TEST_VAULT_DIR = '.e2e_test_vault'
+export const TEST_VAULT_DIR = path.resolve(process.cwd(), '.e2e_test_vault')
